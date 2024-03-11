8a4219d4f3541b5cac9e2ee3354fbb3a1f724faf
lujunji4113 <lujunji4113@gmail.com>
2024-03-11 09:22:09 +0000

    Build environment

--- apps/chrome-extension/rollup.config.mjs
+++ apps/chrome-extension/rollup.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "rollup";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
+import { babel } from "@rollup/plugin-babel";
+import terser from "@rollup/plugin-terser";
 import { globSync } from "glob";
+
+const isDev = process.env.BUILD === "development";
 
 export default defineConfig([
   {
@@ -10,7 +14,11 @@ export default defineConfig([
       file: "bundles/background.js",
       format: "esm",
     },
-    plugins: [typescript()],
+    plugins: [
+      typescript(),
+      babel({ babelHelpers: "bundled" }),
+      ...(isDev ? [] : [terser()]),
+    ],
   },
   ...globSync("src/scripts/*.ts").map((input) => ({
     input,
@@ -18,6 +26,11 @@ export default defineConfig([
       dir: "bundles/scripts",
       format: "iife",
     },
-    plugins: [nodeResolve(), typescript()],
+    plugins: [
+      nodeResolve(),
+      typescript(),
+      babel({ babelHelpers: "bundled" }),
+      ...(isDev ? [] : [terser()]),
+    ],
   })),
 ]);
